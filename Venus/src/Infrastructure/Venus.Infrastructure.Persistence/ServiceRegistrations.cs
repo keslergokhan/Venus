a25738daedb6d78e74901f9d9d9814a089c491b5
keslergokhan <58035013+keslergokhan@users.noreply.github.com>
2025-10-14 23:31:01 +0300

    Fix

---
+++ Venus/src/Infrastructure/Venus.Infrastructure.Persistence/ServiceRegistrations.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Venus.Infrastructure.Persistence.VenusDbContext;
+
+namespace Venus.Infrastructure.Persistence
+{
+    public static class ServiceRegistrationExtensions
+    {
+        public static IServiceCollection AddPersistenceServiceRegistration(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<VenusContext>(x=>x.UseSqlServer(configuration.GetConnectionString("VenusConnection")));
+
+            return services;
+        }
+    }
+}
