f350a61a96e1aba7800734aa1eb7d449a6edc769
keslergokhan <58035013+keslergokhan@users.noreply.github.com>
2025-12-30 00:06:31 +0300

    Blog context kontrolü

--- Venus/src/Presentation/Web/Client/Venus.Presentation.Client.Core/PageTypeServices/VenusDefaultPageTypeService.cs
+++ Venus/src/Presentation/Web/Client/Venus.Presentation.Client.Core/PageTypeServices/VenusDefaultPageTypeService.cs
@@ -1,12 +1,4 @@
-﻿using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Venus.Core.Application.HttpRequests;
-using Venus.Core.Application.HttpRequests.Interfaces;
-using Venus.Presentation.Client.Core.PageTypeServices.Base;
+﻿using Venus.Presentation.Client.Core.PageTypeServices.Base;
 using Venus.Presentation.Client.Core.PageTypeServices.Interfaces;
 
 namespace Venus.Presentation.Client.Core.PageTypeServices
