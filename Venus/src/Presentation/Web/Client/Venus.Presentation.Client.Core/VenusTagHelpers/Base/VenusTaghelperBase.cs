2c9ce178988609ad509971a2b81e267774bf142e
keslergokhan <58035013+keslergokhan@users.noreply.github.com>
2026-06-14 01:30:12 +0300

    fix

---
+++ Venus/src/Presentation/Web/Client/Venus.Presentation.Client.Core/VenusTagHelpers/Base/VenusTaghelperBase.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.DependencyInjection;
+using Scriban;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Venus.Core.Application.HttpRequests.Interfaces;
+using Venus.Presentation.Client.Core.HtmlCustomTagParser;
+
+namespace Venus.Presentation.Client.Core.VenusTagHelpers.Base
+{
+    public abstract class VenusTaghelperBase : TagHelper
+    {
+        protected readonly IMediator Mediator;
+        protected readonly IVenusHttpContext VenusHttpContext;
+        protected readonly IHtmlCustomTagParserAndRenderFactory CustomTagParserAndRenderFactory;
+        protected IServiceProvider ServiceProvider;
+        protected IVenusScribanManager ScribanManager;
+
+        public VenusTaghelperBase(IServiceProvider serviceProvider)
+        {
+            ServiceProvider = serviceProvider;
+            Mediator = ServiceProvider.GetRequiredService<IMediator>();
+            CustomTagParserAndRenderFactory = ServiceProvider.GetRequiredService<IHtmlCustomTagParserAndRenderFactory>();
+            VenusHttpContext = ServiceProvider.GetRequiredService<IVenusHttpContext>();
+            ScribanManager = ServiceProvider.GetRequiredService<IVenusScribanManager>();
+        }
+
+       
+    }
+}
